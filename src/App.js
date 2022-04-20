415048139b58bed27dbee01166461b533d508b4b
luxurydemon123 <luxurydemon123@gmail.com>
2022-04-20 09:53:51 +0700

    initilize

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Switch from "./components/switch/Switch";
+
+function App() {
+  return (
+    <div>
+      <Switch></Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/switch/Switch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Switch = () => {
+  return (
+    <div>
+      <h2>abc</h2>
+    </div>
+  );
+};
+
+export default Switch;
